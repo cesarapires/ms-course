20b07164e08552a387467884f767e6128a02a407
Cesar A. Pires <cesar.augusto.pires@hotmail.com>
2022-06-22 15:56:37 -0300

    hr-payroll implementation mock

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HrPayrollApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrPayrollApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
 
 }
