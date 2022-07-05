00aadda4e2c99cb5713cbbdc912e47ffe79b8768
Cesar A. Pires <cesar.augusto.pires@hotmail.com>
2022-07-05 15:34:58 -0300

    Configuring Zuul api gateway

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,11 @@ package com.devsuperior.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableCircuitBreaker
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
